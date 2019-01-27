b50fad3aef3068a7df9e122de4597e4c2bee73e6
王焱辉 <wyh_19@163.com>
2019-01-27 15:38:58 +0800

    练习增加参数

--- 5-better.py
+++ 5-better.py
@@ -15,13 +15,30 @@ movies = [
     ]
 ]
 
+
 def print_lol(the_list, level=0):
     for item in the_list:
-        if isinstance(item,list):
-            print_lol(item,level+1)
+        if isinstance(item, list):
+            print_lol(item, level+1)
         else:
             for tab_stop in range(level):
-                print("\t",end="")
+                print("\t", end="")
             print(item)
 
+
 print_lol(movies)
+
+# 增加一个控制参数
+
+
+def print_lol2(the_list, indent=False, level=0):
+    for item in the_list:
+        if isinstance(item,list):
+            print_lol2(item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end="")
+            print(item)
+print_lol2(movies)
+print_lol2(movies,True,2)
