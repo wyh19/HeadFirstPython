171fb8bda95bc7e6ccd5f5b3786161bef4b9e86b
王焱辉 <wyh_19@163.com>
2019-01-27 14:48:28 +0800

    练习

--- 1-main.py
+++ 1-main.py
@@ -3,9 +3,39 @@ movies = [
     "The Life of Brian",
     "The Meaning of Life"
 ]
-
 print(movies)
 
-
+# 插入数据
 movies.insert(1, 1975)
-print(movies)
+# 追加数据
+movies.append("Hello")
+# 删除最后一个
+movies.pop()
+# 删除指定索引
+movies.pop(1)
+
+# 元祖 tuple,即不可变的list
+t = ("A","b")
+
+
+# 迭代
+for movie in movies:
+    print(movie)
+
+movies = [
+    "The Holy Grail",
+    "The Life of Brian",
+    "The Meaning of Life",
+    [
+        "Machael Palin",
+        "John CLeeses"
+    ]
+]
+for movie in movies:
+    #类型判断
+    if isinstance(movie,list):
+        for m in movie:
+            print(m)
+    else:
+        print(movie)
+
