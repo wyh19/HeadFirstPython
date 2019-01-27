4094fc02ca1cf82e8c211a06e1a68b9c0389e2a0
王焱辉 <wyh_19@163.com>
2019-01-27 16:02:24 +0800

    增加

---
+++ 6-file-base.py
@@ -0,0 +1,15 @@
+"""练习文件的处理"""
+
+the_file = open('./sketch.txt')
+
+print(the_file.readline(),end='')
+print(the_file.readline(),end='')
+
+print('----以上只是练习-----')
+
+the_file.seek(0)
+
+for line in the_file:
+    print(line,end='')
+
+the_file.close()
---
+++ 7-split.py
@@ -0,0 +1,12 @@
+"""在6中，所有对话都是 xxx: xx的形式"""
+
+data = open('./sketch.txt')
+
+for line in data:
+    (role, spoken) = line.split(':')
+    print(role+' said:', end='')
+    print(spoken, end='')
+
+data.close()
+
+#会发现当出现多个：时，导致值过多的错误
