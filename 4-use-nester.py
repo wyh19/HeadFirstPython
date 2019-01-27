605efd2c83ecda69cb97adf86e28d693c2923429
王焱辉 <wyh_19@163.com>
2019-01-27 15:18:11 +0800

    练习

---
+++ 4-use-nester.py
@@ -0,0 +1,13 @@
+import nester
+
+movies = [
+    "The Holy Grail",
+    "The Life of Brian",
+    "The Meaning of Life",
+    [
+        "Machael Palin",
+        "John CLeeses"
+    ]
+]
+
+nester.print_lol(movies)
