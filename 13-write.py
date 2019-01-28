bd9930fe7d0ae41df1b4786b8d238d1bcd9ffcc8
王焱辉 <wyh_19@163.com>
2019-01-28 11:07:56 +0800

    练习

--- 13-write.py
+++ 13-write.py
@@ -23,7 +23,24 @@ print(man)
 print(other)
 
 #开始写入文件
-out  = open('./file/data.out','w')
-print(man,file = out)
-print(other,file = out)
-out.close()
+try:
+    out  = open('./file/data.out','w')
+    print(man,file = out)
+    print(other,file = out)
+    out.close()
+except IOError:
+    print('File Error!')
+
+# 思考：如果28行代码发生异常，会怎么样？
+# 答案：那么会跳过29行和30行，进入32行，那么由于文件close没有执行，导致文件资源没有释放
+# 解决办法：使用finally确保必须执行的代码得到执行
+try:
+    out  = open('./file/data.out','w')
+    print(man,file = out)
+    print(other,file = out)
+    # 注意，下面一行代码挪到了finally中执行
+    # out.close()
+except IOError:
+    print('File Error!')
+finally:
+    out.close()
