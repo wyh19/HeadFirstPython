d38799e32b09c8aed5f06c35a0ffb77f07180cee
王焱辉 <wyh_19@163.com>
2019-01-28 10:27:42 +0800

    练习

---
+++ 12-except-error.py
@@ -0,0 +1,14 @@
+"""指定特定的异常类型"""
+
+try:
+    data = open('./sketch.txt')
+    for line in data:
+        try:
+            (role, spoken) = line.split(':', 1)
+            print(role+' said:', end='')
+            print(spoken, end='')
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The Data is missing')
