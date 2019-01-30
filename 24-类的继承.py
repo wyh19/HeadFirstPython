6b5fc6ac934dc58716cd69517dfcaa18b0f961e8
王焱辉 <wyh_19@163.com>
2019-01-30 18:57:35 +0800

    练习

---
+++ 24-类的继承.py
@@ -0,0 +1,27 @@
+def sanitize(time_str):
+    if '-' in time_str:
+        spliter = '-'
+    elif ':' in time_str:
+        spliter = ':'
+    else:
+        return time_str
+    (mins, sec) = time_str.split(spliter, 1)
+    return mins + '.' + sec
+
+
+class AthleteList(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    def top3(self):
+        return sorted(set([sanitize(t) for t in self]))[0:3]
+
+
+vera = AthleteList('vera vi')
+vera.append('1.31')
+print(vera.top3())
+vera.extend(['2.22','1-21','2:22'])
+print(vera.top3())
