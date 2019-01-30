4e08bee1487804984033ff35b03cf635b21c55cd
王焱辉 <wyh_19@163.com>
2019-01-30 09:42:03 +0800

    练习

--- 22-class.py
+++ 22-class.py
@@ -1,8 +1,17 @@
+""" 
+练习class
+构造函数为__init__
+每个函数第一个参数必须为self
+"""
+
+
 class Athlete:
     def __init__(self, value=0):
         self.thing = value
+
     def how_big(self):
         return len(self.thing)
+
 
 d = Athlete('Holy Grail')
 print(d.how_big())
