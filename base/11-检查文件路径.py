7ed74aacfb83d159364ccf33a0e1e72eca975099
王焱辉 <wyh_19@163.com>
2019-01-31 16:22:57 +0800

    增加注释

--- base/11-检查文件路径.py
+++ base/11-检查文件路径.py
@@ -1,7 +1,17 @@
-from os.path import exists
+"""
+    1、这一节使用内置库os.path，检查文件是否存在，查看文件的绝对路径
+    2、使用 【 from 模块 import 函数 】的形式
+"""
 
-file_path = './file/sketch.txt'
 
+from os.path import exists, abspath
+
+file_path = './base/data-file/sketch.txt'
+# 打印文件的绝对路径
+print(abspath(file_path))
+print('----分隔线----')
+
+# 判断文件是否存在
 if exists(file_path):
     data = open(file_path)
     for line in data:
