24ea28901d2c7a022cca79f2650a843536bfdbcf
王焱辉 <wyh_19@163.com>
2019-01-31 19:04:42 +0800

    完善

--- base/20-去除列表重复项.py
+++ base/20-去除列表重复项.py
@@ -1,8 +1,15 @@
-"""set用于创建一个没有重复项的列表"""
+"""
+    1、本节的目的是实现列表的去重以及取前3个值
+    2、去重使用 set(xxx_list),得到一个去重后的集合,
+    3、python的集合是{}形式的
+    4、对集合排序，得到一个list
+    5、取集合的前几项使用【 xx_list[x:y] 】形式，x为开始的索引，y为结束的索引（不包括）
+"""
+# 仔细观察下面的输出
+print(set([1, 3, 2, 1, 3, 4, 6, 5]))
+print(sorted(set([1, 3, 2, 1, 3, 4, 6, 5])))
+print(sorted(set([1, 3, 2, 1, 3, 4, 6, 5]))[1:4])
 
-
-
-# 下面是接demo18
 with open('./book-source/hfpy_ch5_data/james.txt') as jaf:
     jm_data = jaf.readline().strip().split(',')
 
@@ -18,4 +25,5 @@ def sanitize(time_str):
     return mins + '.' + sec
 
 
+# 仔细分解下面的语句
 print(sorted(set([sanitize(d) for d in jm_data]))[0:3])
