b389ae4f15ce113d34ba5275b04e5e568b561f85
王焱辉 <wyh_19@163.com>
2019-01-31 15:34:51 +0800

    优化

--- base/10-tyr-except.py
+++ base/10-tyr-except.py
@@ -1,5 +1,10 @@
+"""
+    1、考虑第9节最后提到的问题，如果不写find来判断不含：的语句，怎么让代码不因为报错而中断
+    2、使用python提供的 try-except
+    3、pass 为占位符，表示什么也不执行
+"""
 
-data = open('./file/sketch.txt')
+data = open('./base/data-file/sketch.txt')
 
 for line in data:
     try:
@@ -10,3 +15,5 @@ for line in data:
         pass
 
 data.close()
+
+# 是不是不再报错了
