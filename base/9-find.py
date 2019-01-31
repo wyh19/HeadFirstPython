500bc7930ddf8ffcd944daaa1f3acbd58999a6b3
王焱辉 <wyh_19@163.com>
2019-01-31 15:30:12 +0800

    优化

--- base/9-find.py
+++ base/9-find.py
@@ -1,5 +1,11 @@
+"""
+    1、接上一节，需要检查一条语句中是否包含: ,
+    2、使用字符串的find函数，如果找不到，则返回 -1；如果找到，则返回改字符的索引
+    3、因此判断语句可以写成 if line.find(':') > -1: 
+    也可以写成 if not line.find(':') == -1:
+"""
 
-data = open('./file/sketch.txt')
+data = open('./base/data-file/sketch.txt')
 
 for line in data:
     if line.find(':') > -1:
@@ -9,4 +15,4 @@ for line in data:
 
 data.close()
 
-# if line.find(':') > -1:  可以写成  if not line.find(':') == -1
+# 这次不再报错了
