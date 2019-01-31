2712d5382ede4b20580b362437e739626790b328
王焱辉 <wyh_19@163.com>
2019-01-31 09:57:21 +0800

    练习

---
+++ 25-web/test.py
@@ -0,0 +1,5 @@
+import athletemodel
+
+file_list = ['../book-source/hfpy_ch8_data/james.txt','../book-source/hfpy_ch8_data/julie.txt','../book-source/hfpy_ch8_data/mikey.txt']
+
+print(dir(athletemodel))
