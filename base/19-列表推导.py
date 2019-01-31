d2bff1fa36d89c8891878ce9412d7bd226fff00f
王焱辉 <wyh_19@163.com>
2019-01-31 18:07:06 +0800

    优化

--- base/19-列表推导.py
+++ base/19-列表推导.py
@@ -1,6 +1,17 @@
-"""这一节演示列表推导(list comprehension)"""
-with open('./book-source/hfpy_ch5_data/james.txt') as jaf:
+"""
+1、还记得上一节的下述代码吗？
+clean_jm_data = []
+for d in jm_data:
+    clean_jm_data.append(sanitize(d))
+在python中，可以使用列表推导形式，解决这种简单的列表转化
+形式为：【   [ xxx(item) for item in xx_list ]  】
+
+"""
+
+
+with open('./base/data-file/data1/james.txt') as jaf:
     jm_data = jaf.readline().strip().split(',')
+
 
 def sanitize(time_str):
     if '-' in time_str:
@@ -9,11 +20,11 @@ def sanitize(time_str):
         spliter = ':'
     else:
         return time_str
-    (mins,sec) = time_str.split(spliter,1)
+    (mins, sec) = time_str.split(spliter, 1)
     return mins + '.' + sec
 
-# clean_jm_data = [sanitize(d) for d in jm_data]
-
-# print(sorted(clean_jm_data))
 
 print(sorted([sanitize(d) for d in jm_data]))
+# 如果觉得上面的代码有点吓人，分解为下面两句，习惯就好了
+# clean_jm_data = [sanitize(d) for d in jm_data]
+# print(sorted(clean_jm_data))
