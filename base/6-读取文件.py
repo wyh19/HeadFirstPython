7dd98e4340b29a10fd2f369010b7ac9eb7257052
王焱辉 <wyh_19@163.com>
2019-01-31 14:59:10 +0800

    增加说明

--- base/6-读取文件.py
+++ base/6-读取文件.py
@@ -1,15 +1,32 @@
-"""练习文件的处理"""
+"""
+    1、使用open打开文件
+    2、readline()读取行
+    3、seek(index)将光标移到某个索引位置
+    4、close()关闭文件
+"""
 
-the_file = open('data-file/sketch.txt')
 
-print(the_file.readline(),end='')
-print(the_file.readline(),end='')
+the_file = open('./base/data-file/sketch.txt')
+"""
+上面的文件路径是相对路径，但是根据我的编程经验，应该是 ./data-file/sketch.txt，但是不对
+猜测py文件被执行时，提升到根目录了，所以其同级目录是base
+只是猜测，不要纠结这个问题
+"""
 
+# 打印第一行，光标移到第一行结束
+print(the_file.readline(), end='')
+# 打印第二行
+print(the_file.readline(), end='')
+
+# 分隔线，便于看清楚
 print('----以上只是练习-----')
 
+# 光标回到文件的第一行开始
 the_file.seek(0)
 
+# 依次打印每一行数据
 for line in the_file:
-    print(line,end='')
+    print(line, end='')
 
+# 关闭文件
 the_file.close()
