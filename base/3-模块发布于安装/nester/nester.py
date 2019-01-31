203b1016c3775152c5690715a266f6afd2197a4d
王焱辉 <wyh_19@163.com>
2019-01-31 11:40:40 +0800

    增加说明

--- base/3-模块发布于安装/nester/nester.py
+++ base/3-模块发布于安装/nester/nester.py
@@ -1,5 +1,8 @@
-""" 递归打印嵌套list """
+""" 这里的注释是模块说明 """
+
+
 def print_lol(the_list):
+    """ 这里的注释是函数的说明：递归打印嵌套list """
     for item in the_list:
         if isinstance(item, list):
             print_lol(item)
