81f07393044805e82acc4ab04fd746c73faf5783
王焱辉 <wyh_19@163.com>
2019-01-31 17:24:28 +0800

    优化

--- base/15-with.py
+++ base/15-with.py
@@ -1,4 +1,8 @@
-"""14节中的模式,可以使用with语法专门处理"""
+"""
+    1、对于文件的操作，python提供了with语法，简化try-except-finally的固有模式
+    2、语法形式为 【 with open(xxx) as xx_file 】
+    3、对整个with包上try-except即可处理期间的异常，with会自动帮你关闭文件
+"""
 
 try:
     with open('missing.txt') as data:
